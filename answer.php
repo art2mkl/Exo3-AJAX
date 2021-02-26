a2fa5cb17740a777396aca1c74e0cc4e10a022b9
BAGUIDOU <baguidou@MacBook-Air-de-BAGUIDOU.local>
2021-02-26 14:39:03 +0100

    fin regex

--- answer.php
+++ answer.php
@@ -1,5 +1,19 @@
 <?php
 
+//POST Plaque d'immatriculation
+
+if(isset($_POST['numero'])) {
+    $name = htmlspecialchars($_POST["numero"]);
+    if (!preg_match('/^[a-np-z]{2}-[0-9]{3}-[a-np-z]{2}$/i',$name)) {
+        $nameErr = "Format non valide";
+      } else {
+        $nameErr = "Format valide";
+      }
+      echo $nameErr;
+}
+
+//POST email
+
 if (isset($_POST['email'])) {
     $email = htmlspecialchars($_POST["email"]);
     if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
--- script.js
+++ script.js
@@ -1,3 +1,34 @@
+// INPUT PLAQUE d'IMMATRICULATION
+
+$('#numero').on('input', () => {
+    $.ajax({
+        url: "answer.php",
+        data: {
+            numero: $('#numero').val()
+        },
+        type: "POST",
+        dataType: "Text"
+    })
+
+        .done(function (texte) {
+            if ($('#numero').val().length != 0) {
+                $('.label1').text(texte);
+                if (texte == 'Format non valide') {
+                    $('#numero').css('background-color', 'salmon')
+                    $('.label1').css('color', 'salmon')
+                } else {
+                    $('#numero').css('background-color', 'aquamarine')
+                    $('.label1').css('color', 'aquamarine')
+                }
+            } else {
+                $('.label1').text('');
+                $('#numero').css('background-color', 'white')
+            }
+        })
+})
+
+// INPUT EMAIL
+
 $('#email').on('input', () => {
     $.ajax({
         url: "answer.php",
@@ -11,7 +42,7 @@ $('#email').on('input', () => {
         .done(function (texte) {
             if ($('#email').val().length != 0) {
                 $('.label2').text(texte);
-                if(texte == 'Format non valide') {
+                if (texte == 'Format non valide') {
                     $('#email').css('background-color', 'salmon')
                     $('.label2').css('color', 'salmon')
                 } else {
@@ -24,6 +55,3 @@ $('#email').on('input', () => {
             }
         })
 })
-
-
-
